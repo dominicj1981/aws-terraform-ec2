b3d7fa960827133cd12d32350bd95da4d4fa73ab
dominicj1981 <113531177+dominicj1981@users.noreply.github.com>
2022-10-28 18:49:05 +0530

    Update variables.tf

--- app-servers/variables.tf
+++ app-servers/variables.tf
@@ -1,33 +1,33 @@
 variable "count" {
-  type = "string"
+  type = string
   description = "How many EC2 instances to deploy"
 }
 
 variable "instance_name" {
-    type = "string"
+    type = string
     description = "Instance Name"
 }
 
 
 variable "aws_region" {
-    type = "string"
+    type = string
     description = "Region to create resources"
 }
 
 
 variable "instance_type" {
-    type = "string"
+    type = string
     description = "EC2 instance type"
 }
 
 variable "key_name" {
-    type = "string"
+    type = string
     description = "key-name to deploy with"
 }
 
 
 variable "subnet_id" {
-    type = "string"
+    type = string
     description = "Resource Subnet ID"
 }
 
